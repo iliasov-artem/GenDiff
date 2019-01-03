6eca0349ce6738ee39ad6c384bb20bf52a37812b
artem-iliasov <iliasov.artem.27@gmail.com>
2019-01-03 13:19:09 +0300

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,8 @@
-import fs from 'fs';
-import gendiff from '../src';
 
-const expected = fs.readFileSync('__tests__/__fixtures__/result.txt');
+const fs = require('fs');
+const gendiff = require('../src').default;
+
+const expected = fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf-8');
 
 test('Compare JSON', () => {
   expect(gendiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json')).toBe(expected);
--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ const getDiff = (file1, file2) => {
   const allKeys = Object.keys({ ...file1, ...file2 });
   const result = allKeys.reduce((acc, key) => {
     if (!has(file1, key)) {
-      return [...acc, `+ ${key}: ${file2[key]}`];
+      return [...acc, `  + ${key}: ${file2[key]}`];
     }
     if (!has(file2, key)) {
-      return [...acc, `- ${key}: ${file1[key]}`];
+      return [...acc, `  - ${key}: ${file1[key]}`];
     }
     if (file1[key] === file2[key]) {
-      return [...acc, ` ${key}: ${file1[key]}`];
+      return [...acc, `    ${key}: ${file1[key]}`];
     }
-    return [...acc, `+ ${key}: ${file2[key]}\n- ${key}: ${file1[key]}`];
+    return [...acc, `  + ${key}: ${file2[key]}\n  - ${key}: ${file1[key]}`];
   }, []);
-  return `{\n${result.join('\n')}\n}`;
+  return `{\n${result.join('\n')}\n}\n`;
 };
 
 const genDiff = (path1, path2) => {
