e6884c52ab1ab60515965424a9d62e486157934e
artem-iliasov <iliasov.artem.27@gmail.com>
2019-01-03 17:26:07 +0300

    Add comparing ini

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,10 +3,14 @@
 import fs from 'fs';
 import gendiff from '../src';
 
-test('Compare JSON', () => {
+test('Comparing JSON', () => {
   expect(gendiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json')).toBe(fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf-8'));
 });
 
-test('Compare yaml', () => {
+test('Comparing yaml', () => {
   expect(gendiff('__tests__/__fixtures__/before.yaml', '__tests__/__fixtures__/after.yaml')).toBe(fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf-8'));
+});
+
+test('Comparing ini', () => {
+  expect(gendiff('__tests__/__fixtures__/before.ini', '__tests__/__fixtures__/after.ini')).toBe(fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf-8'));
 });
--- src/parsers.js
+++ src/parsers.js
@@ -1,15 +1,16 @@
 import yaml from 'js-yaml';
 import path from 'path';
 import fs from 'fs';
+import ini from 'ini';
 
 const actionsStore = {
   json: data => JSON.parse(data),
   yaml: data => yaml.safeLoad(data),
+  ini: data => ini.decode(data),
 };
 
 export default (filePath) => {
-  const fileContent = fs.readFileSync(filePath);
+  const fileContent = fs.readFileSync(filePath, 'utf-8');
   const extension = path.extname(filePath).slice(1);
-  console.log(extension);
   return actionsStore[extension](fileContent);
 };
